96c3a08a853e44c0ce05c8819efea1d461a9bd58
Maksym <maksymRozhko7@gmail.com>
2022-03-08 01:12:39 +0200

    feat: modals, update showModalMyTime

--- dist/script.js
+++ dist/script.js
@@ -90,10 +90,105 @@
 /*!************************!*\
   !*** ./src/js/main.js ***!
   \************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _modules_modals__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/modals */ "./src/js/modules/modals.js");
 
+window.addEventListener('DOMContentLoaded', () => {
+  Object(_modules_modals__WEBPACK_IMPORTED_MODULE_0__["default"])();
+});
+
+/***/ }),
+
+/***/ "./src/js/modules/modals.js":
+/*!**********************************!*\
+  !*** ./src/js/modules/modals.js ***!
+  \**********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+const modals = () => {
+  function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+    const trigger = document.querySelectorAll(triggerSelector);
+    const modal = document.querySelector(modalSelector);
+    const close = document.querySelector(closeSelector);
+    const windows = document.querySelectorAll('[data-modal');
+
+    function showModal() {
+      modal.classList.add('show');
+      document.body.classList.add('modal-open');
+    }
+
+    ;
+
+    function closeModal() {
+      modal.classList.remove('show');
+      document.body.classList.remove('modal-open');
+    }
+
+    ;
+    trigger.forEach(btn => {
+      btn.addEventListener('click', e => {
+        const target = e.target;
+
+        if (target) {
+          e.preventDefault();
+        }
+
+        ;
+        windows.forEach(item => item.classList.remove('show'));
+        showModal();
+      });
+    });
+    close.addEventListener('click', () => {
+      closeModal();
+      windows.forEach(item => item.classList.remove('show'));
+    });
+    modal.addEventListener('click', e => {
+      const target = e.target;
+
+      if (target === modal && closeClickOverlay) {
+        windows.forEach(item => item.classList.remove('show'));
+        closeModal();
+      }
+    });
+  }
+
+  ;
+
+  function showModalByTime(modalSelector, time) {
+    const modal = document.querySelector(modalSelector);
+    setTimeout(() => {
+      let state;
+      document.querySelectorAll('[data-modal]').forEach(item => {
+        if (getComputedStyle(item).display !== 'none') {
+          state = true;
+        }
+
+        ;
+      });
+
+      if (!state) {
+        modal.classList.add('show');
+        document.body.classList.add('modal-open');
+      }
+
+      ;
+    }, time);
+  }
+
+  bindModal('.button-order.button-design', '.popup-design', '.popup-design .popup-close', false);
+  bindModal('.button-order.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
+  bindModal('.fixed-gift', '.popup-gift', '.popup-gift .popup-close');
+  showModalByTime('.popup-consultation', 5000);
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (modals);
 
 /***/ })
 
---
+++ src/js/modules/modals.js
@@ -0,0 +1,72 @@
+const modals = () => {
+    function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+        const trigger = document.querySelectorAll(triggerSelector);
+        const modal = document.querySelector(modalSelector);
+        const close = document.querySelector(closeSelector);
+        const windows = document.querySelectorAll('[data-modal');
+
+        function showModal() {
+            modal.classList.add('show');
+            document.body.classList.add('modal-open');
+        };
+
+        function closeModal() {
+            modal.classList.remove('show');
+            document.body.classList.remove('modal-open');
+        };
+
+        trigger.forEach(btn => {
+            btn.addEventListener('click', e => {
+                const target = e.target;
+
+                if (target) {
+                    e.preventDefault();
+                };
+
+                windows.forEach(item => item.classList.remove('show'));
+                
+                showModal();
+            });
+        });
+
+        close.addEventListener('click', () => {
+            closeModal();
+            windows.forEach(item => item.classList.remove('show'));
+        });
+
+        modal.addEventListener('click', e => {
+            const target = e.target;
+
+            if (target === modal && closeClickOverlay) {
+                windows.forEach(item => item.classList.remove('show'));
+                closeModal();
+            }
+        });
+    };
+
+    function showModalByTime(modalSelector, time) {
+        const modal = document.querySelector(modalSelector);
+        
+        setTimeout(() => {
+            let state;
+
+            document.querySelectorAll('[data-modal]').forEach(item => {
+                if (getComputedStyle(item).display !== 'none') {
+                    state = true;
+                };
+            });
+
+            if (!state) {
+                modal.classList.add('show');
+                document.body.classList.add('modal-open');
+            };
+        }, time);
+    }
+
+    bindModal('.button-order.button-design', '.popup-design', '.popup-design .popup-close', false);
+    bindModal('.button-order.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
+    bindModal('.fixed-gift', '.popup-gift', '.popup-gift .popup-close');
+    showModalByTime('.popup-consultation', 5000);
+};
+
+export default modals;
