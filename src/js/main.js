3eba3a3c858bee55155e085cae6c1ab1a3966e67
Maksym <maksymRozhko7@gmail.com>
2022-03-08 23:27:39 +0200

    feat: form check inputs text, and mask for input phone

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
+import checkTextInputs from "./modules/checkTextInputs";
 import forms from "./modules/forms";
+import mask from "./modules/mask";
 import modals from "./modules/modals";
 import sliders from "./modules/sliders";
 
@@ -8,4 +10,7 @@ window.addEventListener('DOMContentLoaded', () => {
     sliders('.main-slider-item', 'vertical', '', '', loopSlidesSeconds);
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn', loopSlidesSeconds);
     forms();
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
 });
